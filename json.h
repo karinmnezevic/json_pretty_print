1039eb1180d1867dd2d5d8182e32a9c0a37e8191
karinmnezevic <30531106+karinmnezevic@users.noreply.github.com>
2023-12-20 02:24:36 +0100

    Add files via upload

--- json.h
+++ json.h
@@ -4,68 +4,107 @@
 #include <memory>
 #include <iostream>
 
+int indentation = 2;
+
 class json_any {
 public:
-    virtual void print() const = 0;
+    virtual void print(int level) const = 0;
+    virtual size_t len() const = 0;
     virtual ~json_any() = default;
 };
 
 class json_object : public json_any {
-    std::vector<std::pair<std::string, std::unique_ptr<json_any>>> _key_vals;
+    std::vector<std::pair<std::string_view, std::unique_ptr<json_any>>> _key_vals;
 public:
-    json_object(std::vector<std::pair<std::string, std::unique_ptr<json_any>>>& key_vals) {
-        for (auto& ptr : key_vals) 
-            _key_vals.push_back(std::move(ptr));
-    }
-    void print() const override {
-        std::cout << '{' << std::endl;
-        for (const auto& [key, val_ptr] : _key_vals) {
-            std::cout << key << ": ";
-            val_ptr->print();
-            std::cout << ", ";
+    json_object(std::vector<std::pair<std::string_view, std::unique_ptr<json_any>>>& key_vals) :
+        _key_vals(std::move(key_vals)) {}
+
+    void print(int level) const override {
+        bool new_lines = len() > 60 || _key_vals.size() > 4;
+        std::cout << '{';
+        if (new_lines)
+            std::cout << std::endl;
+
+        for (int i = 0; i < _key_vals.size(); i ++) {
+            if (new_lines)
+                std::cout << std::string(level * indentation, ' ');
+            std::cout << _key_vals[i].first << ": ";
+            _key_vals[i].second->print(level + 1);
+            if (i != _key_vals.size() - 1)
+                std::cout << ", ";
+            if (new_lines)
+                std::cout << std::endl;
         }
-        std::cout << '}' << std::endl;
+        if (new_lines)
+            std::cout << std::string((level-1) * indentation, ' ');
+        std::cout << '}';
+    }
+    size_t len() const {
+        int ret = 2; // {}
+        for (const auto& [key, val] : _key_vals) {
+            ret += key.length() + 2; // : 
+            ret += val->len() + 2; // ,
+        }
+        return ret - 2; // no comma after last elem
     }
 };
 
 class json_array : public json_any {
     std::vector<std::unique_ptr<json_any>> _array;
 public:
-    json_array(std::vector<std::unique_ptr<json_any>>& array) {
-        for (auto& ptr : array)
-            _array.push_back(std::move(ptr));
-    }
-    void print() const override {
-        std::cout << '[' << std::endl;
-        for (const auto& ptr : _array) {
-            ptr->print();
-            std::cout << ", ";
+    json_array(std::vector<std::unique_ptr<json_any>>& array) :
+        _array(std::move(array)) {}
+
+    void print(int level) const override {
+        bool new_lines = len() > 60 || _array.size() > 4;
+        std::cout << '[';
+        if (new_lines)
+            std::cout << std::endl;
+
+        for (int i = 0; i < _array.size(); i ++) {
+            if (new_lines)
+                std::cout << std::string(level * indentation, ' ');
+            _array[i]->print(level + 1);
+            if (i != _array.size() - 1)
+                std::cout << ", ";
+            if (new_lines)
+                std::cout << std::endl;
         }
-        std::cout << ']' << std::endl;
+        if (new_lines)
+            std::cout << std::string((level-1) * indentation, ' ');
+        std::cout << ']';
+    }
+    size_t len() const {
+        int ret = 2; // []
+        for (const auto& elem : _array)
+            ret += elem->len() + 2;  //,
+        return ret - 2; // no comma after last elem
     }
 };
 
-//json_val<string> -> json_val<string_view>
 template <typename T>
 class json_val : public json_any {
-    T _val;
+    std::string_view _val;
 public:
-    json_val(T val) : _val(val) {}
-    void print() const override {
+    json_val(char * beg, size_t len) : _val(beg, len) {}
+    void print(int) const override {
         std::cout << _val;
     };
-    T get() const {
+    std::string_view get() const {
         return _val;
+    }
+    size_t len() const {
+        return _val.length();
     }
 };
 
 class json_parser {
 private:
-    std::string_view _expr;
+    std::string _expr;
     size_t _pos;
 
 public:
-    json_parser(const std::string_view expr) : _expr(expr), _pos(0) {}
+    json_parser(const std::string expr) : _expr(expr), _pos(0) {}
     std::unique_ptr<json_any> parse() {
         skip_spaces();
         return parse_any();
@@ -106,10 +145,10 @@ private:
     }
     
     std::unique_ptr<json_object> parse_object() {
-        std::vector<std::pair<std::string, std::unique_ptr<json_any>>> result;
+        std::vector<std::pair<std::string_view, std::unique_ptr<json_any>>> result;
         while (next_char() != '}') {
             skip_spaces();
-            std::string key = parse_string()->get();
+            std::string_view key = parse_string()->get();
             next_char();
             skip_spaces();
             result.push_back(std::make_pair(key, parse_any()));
@@ -130,38 +169,40 @@ private:
     }
 
     std::unique_ptr<json_val<std::string>> parse_string() {
-        std::string result;
+        size_t beg = _pos;
         next_char();
         while (_pos < _expr.size() && peek_next_char() != '\"') {
             // Handle escape characters
-            result += next_char();
+            next_char();
         }
         next_char();
-        return std::make_unique<json_val<std::string>>(result);
+        return std::make_unique<json_val<std::string>>(_expr.data() + beg, _pos - beg);
     }
 
     std::unique_ptr<json_val<bool>> parse_bool() {
         if (peek_next_char() == 't') {
             _pos += 4;
-            return std::make_unique<json_val<bool>>(true);
+            return std::make_unique<json_val<bool>>(_expr.data() + _pos - 4, 4);
         }
         else {
             _pos += 5;
-            return std::make_unique<json_val<bool>>(false);
+            return std::make_unique<json_val<bool>>(_expr.data() + _pos - 5, 5);
         }
     }
 
     std::unique_ptr<json_val<std::nullptr_t>> parse_null() {
         _pos += 4;
-        return std::unique_ptr<json_val<std::nullptr_t>>(nullptr);
+        return std::make_unique<json_val<std::nullptr_t>>(_expr.data() + _pos - 4, 4);
     }
 
     std::unique_ptr<json_val<int>> parse_number() {
-        //dodati neku podrsku za float?
-        std::string number;
-        while (_pos < _expr.size() && std::isdigit(peek_next_char())) 
-            number += next_char();
+        size_t beg = _pos;
 
-        return std::make_unique<json_val<int>>(stoi(number));
+        while (_pos < _expr.size() && 
+                (std::isdigit(peek_next_char()) || peek_next_char() == '-' || peek_next_char() == '.' || 
+                peek_next_char() == 'e' || peek_next_char() == 'E')) 
+            next_char();
+
+        return std::make_unique<json_val<int>>(_expr.data() + beg, _pos - beg);
     }
 };
--- main.cpp
+++ main.cpp
@@ -3,22 +3,16 @@
 
 int main() {
     // Example usage
-    std::string json_input = R"(
-        {
-            "name": "John Doe",
-            "age": 30,
-            "isStudent": false,
-            "grades": [95, 88, 75],
-            "address": {
-                "city": "New York",
-                "zip": "10001"
-            }
-        }
-    )";
-    
-    json_parser parser(json_input);
-    std::unique_ptr<json_any> parsed_json = parser.parse();
-    parsed_json->print();
-
+    //{"name": "John Doe", "age": 30,"isStudent": false,"grades": [95, 88, 75],"address": {"city": "New York","zip": "10001"}}
+    //{"coord": [{"x": 1.2, "y": 1.23}, {"x": 3.5432, "y": 123}, {"x": 1, "y": 123.4} , {"x": -12, "y": 1.2}, {"x": 12, "y": 969.696}]}
+    //{"coord": [[1.2, 1.23], [-3.1415, 2], [0.3, 2], [12, 969.696], [1, 2, 3], [-1, -2, -3, -4]]}
+    std::string json_input;
+    while (2 < 3) {
+        getline(std::cin, json_input);
+        json_parser parser(json_input);
+        std::unique_ptr<json_any> parsed_json = parser.parse();
+        parsed_json->print(1);
+        std::cout << parsed_json->len() << std::endl;
+    }
     return 0;
 }
